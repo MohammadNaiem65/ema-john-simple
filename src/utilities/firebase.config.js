47426eec47bd1bb3517cd4909927b4b3517646fd
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2022-10-25 12:38:07 +0600

    completed the initial setup

---
+++ src/utilities/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDsnx0w0Yk2d1do91R6HzbfoJFYBnztHIU",
+    authDomain: "ema-john-simple-2fb4a.firebaseapp.com",
+    projectId: "ema-john-simple-2fb4a",
+    storageBucket: "ema-john-simple-2fb4a.appspot.com",
+    messagingSenderId: "330463429581",
+    appId: "1:330463429581:web:237e3e3d9b80beeb3bba84"
+};
+
+export const app = initializeApp(firebaseConfig);
